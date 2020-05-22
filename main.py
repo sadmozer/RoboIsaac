46443b90a73bc12b258c0b77a23a4cbafd80b4b3
NICCOLO' CARDELLI <n.cardelli2@studenti.unipi.it>
2020-05-22 15:22:50 +0200

    Meta info & minimal minimap detection

--- main.py
+++ main.py
@@ -11,117 +11,63 @@ from os.path import isfile, join, isdir
 import random
 import json
 from mss import mss
-def process(original_image):
-    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    # th1=50, th2=100 OTTIMO
-    # processed_img = cv2.Canny(processed_img, threshold1=50, threshold2=100)
-    return processed_img
-
-def region_of_interest(img, vertices):
-    mask = np.zeros_like(img)
-    cv2.fillPoly(mask, vertices, 255)
-    masked = cv2.bitwise_and(img, mask)
-    return masked
-
-max_methods = [eval(m) for m in ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR','cv2.TM_CCORR_NORMED']]
-min_methods = [eval(m) for m in ['cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']]
-def detect_img(img, pattern, confidence, mode):
-    dim = pattern.shape
-    res = cv2.matchTemplate(img, pattern, mode) 
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    top_left = None
-    bottom_right = None
-    val = None
-    if(mode in max_methods and max_val > confidence):
-        top_left = max_loc
-        bottom_right = (top_left[0] + dim[1], top_left[1] + dim[0])
-        val = max_val
-    elif(mode in min_methods and min_val < 1-confidence):
-        top_left = min_loc
-        bottom_right = (top_left[0] + dim[1], top_left[1] + dim[0])
-        val = 1-min_val
-    return top_left, bottom_right, val
 
 def import_images(path):
     images = {}
     for f1 in listdir(path):
         folder_path = join(path, f1)
         if(isdir(folder_path)):
-            images_in_dir =  {'images': {}, 'bounds': {}}
+            images_in_dir =  {'images': {}}
+            with open(join(folder_path, 'meta.json')) as b:
+                meta = json.load(b)
+                images_in_dir['bounds'] = np.array(meta['bounds'])
+                images_in_dir['img_color'] = eval(f"cv2.COLOR_BGRA2{meta['img_color']}")
+                images_in_dir['mode'] = eval(meta['mode'])
+                images_in_dir['threshold'] = meta['threshold']
+                images_in_dir['legend_color'] = meta['legend_color']
             for f2 in listdir(folder_path):
                 file_path = join(folder_path, f2)
-                if(isfile(file_path)):
-                    if(f2.split(".")[-1].lower() == 'png'):
-                        img = cv2.imread(file_path)
-                        images_in_dir['images'][f2.split(".")[0]] = process(img)
-                    else:
-                        with open(join(folder_path, 'bounds.json')) as b:
-                            images_in_dir['bounds'] = np.array([json.load(b)['bounds']])
+                file_name = f2.split(".")[0]
+                file_format = f2.split(".")[-1].lower()
+                if(isfile(file_path) and file_format == 'png'):
+                    img = cv2.imread(file_path, flags=cv2.IMREAD_UNCHANGED)
+                    images_in_dir['images'][file_name] = cv2.cvtColor(img, images_in_dir['img_color'])
             images[f1] = images_in_dir
     return images
 
-def enable_detection(screen, images):
-    for im in images:
-        confidence = 0
-        top_left = None
-        i=0
-        curr_imgs = images[im]['images']
-        names = list(curr_imgs.keys())
-        vals = list(curr_imgs.values())
-        while(i < len(names) and not top_left):
-            top_left, bottom_right, confidence = detect_img(
-                img=region_of_interest(screen, images[im]['bounds']), 
-                pattern=vals[i],
-                confidence=0.9,
-                mode=cv2.TM_CCOEFF_NORMED)
-            if(not top_left):
-                i+=1
-        if(i < len(names)):
-            cv2.rectangle(screen, top_left, bottom_right, (255, 255, 0), 1)
-            cv2.putText(screen, names[i], (top_left[0], top_left[1]-3), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 0), 1, cv2.LINE_AA)
-
 cv2.namedWindow('window')
-cv2.moveWindow('window', -1000, 20)
-cv2.imshow('window', np.zeros((504, 947, 3)))
+# cv2.resizeWindow('window', 400, 225)
+# # cv2.moveWindow('window', 950, 20)
+# cv2.imshow('window', np.zeros((504, 947, 3)))
 
 images = import_images('.\images')
-setupgame.StartGame()
-
 kkeys = [W, A, S, D]
 timer = 0
 rand = random.randint(0, 3)
+monitor = {"top": 33, "left": 9, "width": 944, "height": 500}
 
+if __name__ == "__main__":
+    setupgame.StartGame() 
+    while(True):
+        with mss() as sct:
+            screen = np.array(sct.grab(monitor))
+            setupgame.StartDetection(screen, images)
+            # screen = cv2.cvtColor(screen, cv2.COLOR_BGRA2RGB)
+            cv2.imshow('window', screen)
 
-with mss() as sct:
-    # Part of the screen to capture
-    monitor = {"top": 33, "left": 9, "width": 944, "height": 500}
+            if(cv2.waitKey(25) & 0xFF == ord("q")):
+                cv2.destroyAllWindows()
+                break
 
-    while "Screen capturing":
-
-        # Get raw pixels from the screen, save it to a Numpy array
-        screen = np.array(sct.grab(monitor))
-
-        gray_screen = process(screen)
-        enable_detection(gray_screen, images)
-        
-        # Display the picture
-        cv2.imshow('window', gray_screen)
-
-        # Press "q" to quit
-        if(cv2.waitKey(25) & 0xFF == ord("q")):
-            cv2.destroyAllWindows()
-            break
-
-        if(timer<rand):
-            timer+=1
-        else:
-            ReleaseKey(kkeys[rand])
-            timer = 0
-            rand = random.randint(0, 3)
-            PressKey(kkeys[rand])
+        # if(timer<rand):
+        #     timer+=1
+        # else:
+        #     ReleaseKey(kkeys[rand])
+        #     timer = 0
+        #     rand = random.randint(0, 3)
+        #     PressKey(kkeys[rand])
         # screen = np.array(ImageGrab.grab(bbox=(7, 30, 954, 534)))
-        # new_screen = process(screen)
-        # cv2.imshow('window', new_screen)
+
 
 
 
--- setupgame.py
+++ setupgame.py
@@ -1,5 +1,6 @@
 import subprocess, sys
-
+import numpy as np
+import cv2
 def StartGame():
     p = subprocess.Popen(["powershell.exe",
     "-ExecutionPolicy",
@@ -10,7 +11,59 @@ def StartGame():
     out, err = p.communicate()
     try:
         print(f"[SETUP-STDOUT]: {out.decode('utf-8').strip()}")
-        print(f"[SETUP-STDERR]: {err.decode('utf-8').strip()}")
     except Exception as e:
+        print(f"[SETUP-STDERR]: {err.decode('utf-8').strip()}")
         print(e)
-    
+
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+def process(original_image, color):
+    processed_img = cv2.cvtColor(original_image, color)
+    # th1=50, th2=100 OTTIMO
+    # processed_img = cv2.Canny(processed_img, threshold1=50, threshold2=100)
+    return processed_img
+
+max_methods = [eval(m) for m in ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR','cv2.TM_CCORR_NORMED']]
+min_methods = [eval(m) for m in ['cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']]
+def detect_img(img, pattern, confidence, mode):
+    dim = pattern.shape
+    res = cv2.matchTemplate(img, pattern, mode) 
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    top_left = None
+    bottom_right = None
+    val = None
+    if(mode in max_methods and max_val > confidence):
+        top_left = max_loc
+        bottom_right = (top_left[0] + dim[1], top_left[1] + dim[0])
+        val = max_val
+    elif(mode in min_methods and min_val < 1-confidence):
+        top_left = min_loc
+        bottom_right = (top_left[0] + dim[1], top_left[1] + dim[0])
+        val = 1-min_val
+    return top_left, bottom_right, val
+
+def StartDetection(screen, images):
+    for im in images:
+        confidence = 0
+        top_left = None
+        i=0
+        dict_imgs = images[im]['images']
+        names = list(dict_imgs.keys())
+        vals = list(dict_imgs.values())
+        while(i < len(names) and not top_left):
+            processed_screen = process(screen, images[im]['img_color'])
+            processed_screen = region_of_interest(processed_screen, images[im]['bounds'])
+            top_left, bottom_right, confidence = detect_img(
+                img=processed_screen, 
+                pattern=vals[i],
+                confidence=images[im]['threshold'],
+                mode=images[im]['mode'])
+            if(not top_left):
+                i+=1
+        if(i < len(names)):
+            cv2.rectangle(screen, top_left, bottom_right, images[im]['legend_color'], 1)
+            cv2.putText(screen, f"{names[i][:1]}:{confidence:.5f}", (top_left[0]-5, top_left[1]-3), cv2.FONT_HERSHEY_SIMPLEX, 0.3, images[im]['legend_color'], 1, cv2.LINE_AA)
